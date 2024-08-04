0f049fb344771bea09cd66a47045d4403f60499a
BlenWBegashaw <133809716+BlenWBegashaw@users.noreply.github.com>
2024-08-03 17:04:19 -0700

    Update caseybot.js

--- force-app/main/default/lwc/caseybot/caseybot.js
+++ force-app/main/default/lwc/caseybot/caseybot.js
@@ -1,3 +1,48 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import CASE_SUBJECT_FIELD from '@salesforce/schema/Case.Subject';
+import CASE_DESCRIPTION_FIELD from '@salesforce/schema/Case.Description';
 
-export default class Caseybot extends LightningElement {}
+const FIELDS = [CASE_SUBJECT_FIELD, CASE_DESCRIPTION_FIELD];
+
+export default class CaseRecommendationBot extends LightningElement {
+    @api recordId;
+    caseSubject;
+    caseDescription;
+    recommendations = [];
+
+    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
+    case({ error, data }) {
+        if (data) {
+            this.caseSubject = data.fields.Subject.value;
+            this.caseDescription = data.fields.Description.value;
+            this.fetchRecommendations();
+        } else if (error) {
+            console.error(error);
+        }
+    }
+
+    fetchRecommendations() {
+        const payload = {
+            case: {
+                subject: this.caseSubject,
+                description: this.caseDescription
+            }
+        };
+
+        fetch('https://your-app-name.herokuapp.com/match_cases', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        })
+        .then(response => response.json())
+        .then(data => {
+            this.recommendations = data;
+        })
+        .catch(error => {
+            console.error('Error fetching recommendations:', error);
+        });
+    }
+}
