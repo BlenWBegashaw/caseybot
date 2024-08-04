e612f7f60e277185d21661335afdc50d30da42d8
BlenWBegashaw <133809716+BlenWBegashaw@users.noreply.github.com>
2024-08-03 22:35:28 -0700

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Authenticate with Salesforce
-sfdx auth:jwt:grant --clientid $SF_CLIENT_ID --jwtkeyfile server.key --username $SF_USERNAME --instanceurl https://login.salesforce.com --setdefaultdevhubusername
-
-# Deploy the source to Salesforce
-sfdx force:source:deploy -p force-app -u $SF_USERNAME
