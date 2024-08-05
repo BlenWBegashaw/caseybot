4007aeab650a9aab48730afff1c367b1335e0c5a
BlenWBegashaw <133809716+BlenWBegashaw@users.noreply.github.com>
2024-08-05 13:57:57 -0700

    Update caseybot.js

--- force-app/main/default/lwc/caseybot/caseybot.js
+++ force-app/main/default/lwc/caseybot/caseybot.js
@@ -1,6 +1,75 @@
+// import { LightningElement, api, wire } from 'lwc';
+// import { subscribe, MessageContext } from 'lightning/messageService';
+// import SAMPLEMC from '@salesforce/messageChannel/SampleMessageChannel__c';
+
+// export default class Caseybot extends LightningElement {
+//     @api recordId;
+//     caseSubject;
+//     caseDescription;
+//     recommendations = [];
+//     showModal = false;
+//     subscription = null;
+
+//     @wire(MessageContext)
+//     messageContext;
+
+//     connectedCallback() {
+//         this.subscribeToMessageChannel();
+//     }
+
+//     subscribeToMessageChannel() {
+//         if (!this.subscription) {
+//             this.subscription = subscribe(
+//                 this.messageContext,
+//                 SAMPLEMC,
+//                 (message) => this.handleMessage(message)
+//             );
+//         }
+//     }
+
+//     handleMessage(message) {
+//         this.caseSubject = message.subject;
+//         this.caseDescription = message.description;
+//     }
+
+//     handleFetchRecommendations() {
+//         const payload = {
+//             case: {
+//                 subject: this.caseSubject,
+//                 description: this.caseDescription
+//             }
+//         };
+
+//         console.log('Payload:', payload); // Log the payload to verify
+
+//         fetch('https://caseybot-3785eca7c1f1.herokuapp.com/match_cases', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(payload)
+//         })
+//         .then(response => response.json())
+//         .then(data => {
+//             this.recommendations = data;
+//             this.showModal = true;
+//             console.log('Recommendations:', this.recommendations);
+//         })
+//         .catch(error => {
+//             console.error('Error fetching recommendations:', error);
+//         });
+//     }
+
+//     handleCloseModal() {
+//         this.showModal = false;
+//     }
+// }
 import { LightningElement, api, wire } from 'lwc';
-import { subscribe, MessageContext } from 'lightning/messageService';
-import SAMPLEMC from '@salesforce/messageChannel/SampleMessageChannel__c';
+import { getRecord } from 'lightning/uiRecordApi';
+import CASE_SUBJECT_FIELD from '@salesforce/schema/Case.Subject';
+import CASE_DESCRIPTION_FIELD from '@salesforce/schema/Case.Description';
+
+const FIELDS = [CASE_SUBJECT_FIELD, CASE_DESCRIPTION_FIELD];
 
 export default class Caseybot extends LightningElement {
     @api recordId;
@@ -8,35 +77,24 @@ export default class Caseybot extends LightningElement {
     caseDescription;
     recommendations = [];
     showModal = false;
-    subscription = null;
 
-    @wire(MessageContext)
-    messageContext;
-
-    connectedCallback() {
-        this.subscribeToMessageChannel();
-    }
-
-    subscribeToMessageChannel() {
-        if (!this.subscription) {
-            this.subscription = subscribe(
-                this.messageContext,
-                SAMPLEMC,
-                (message) => this.handleMessage(message)
-            );
+    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
+    case({ error, data }) {
+        if (data) {
+            this.caseSubject = data.fields.Subject.value;
+            this.caseDescription = data.fields.Description.value;
+        } else if (error) {
+            console.error(error);
         }
     }
 
-    handleMessage(message) {
-        this.caseSubject = message.subject;
-        this.caseDescription = message.description;
-    }
-
     handleFetchRecommendations() {
+        const currentUrl = window.location.href; // Get the current URL
         const payload = {
             case: {
                 subject: this.caseSubject,
-                description: this.caseDescription
+                description: this.caseDescription,
+                url: currentUrl // Include the current URL in the payload
             }
         };
 
