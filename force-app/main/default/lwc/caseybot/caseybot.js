40b31a4ac3f98d11c8ba8c1bc2a1b9f85b9e0f22
BlenWBegashaw <133809716+BlenWBegashaw@users.noreply.github.com>
2024-08-05 15:17:22 -0700

    Update caseybot.js

--- force-app/main/default/lwc/caseybot/caseybot.js
+++ force-app/main/default/lwc/caseybot/caseybot.js
@@ -64,6 +64,64 @@
 //         this.showModal = false;
 //     }
 // }
+// import { LightningElement, api, wire } from 'lwc';
+// import { getRecord } from 'lightning/uiRecordApi';
+// import CASE_SUBJECT_FIELD from '@salesforce/schema/Case.Subject';
+// import CASE_DESCRIPTION_FIELD from '@salesforce/schema/Case.Description';
+
+// const FIELDS = [CASE_SUBJECT_FIELD, CASE_DESCRIPTION_FIELD];
+
+// export default class Caseybot extends LightningElement {
+//     @api recordId;
+//     caseSubject;
+//     caseDescription;
+//     recommendations = [];
+//     showModal = false;
+
+//     @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
+//     case({ error, data }) {
+//         if (data) {
+//             this.caseSubject = data.fields.Subject.value;
+//             this.caseDescription = data.fields.Description.value;
+//         } else if (error) {
+//             console.error(error);
+//         }
+//     }
+
+//     handleFetchRecommendations() {
+//         const currentUrl = window.location.href; // Get the current URL
+//         const payload = {
+//             case: {
+//                 subject: this.caseSubject,
+//                 description: this.caseDescription,
+//                 url: currentUrl // Include the current URL in the payload
+//             }
+//         };
+
+//         console.log('Payload:', payload); // Log the payload to verify
+
+//         fetch('https://caseybot-3785eca7c1f1.herokuapp.com/match_cases', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(payload)
+//         })
+//         .then(response => response.json())
+//         .then(data => {
+//             this.recommendations = data;
+//             this.showModal = true;
+//             console.log('Recommendations:', this.recommendations);
+//         })
+//         .catch(error => {
+//             console.error('Error fetching recommendations:', error);
+//         });
+//     }
+
+//     handleCloseModal() {
+//         this.showModal = false;
+//     }
+// }
 import { LightningElement, api, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 import CASE_SUBJECT_FIELD from '@salesforce/schema/Case.Subject';
@@ -90,22 +148,17 @@ export default class Caseybot extends LightningElement {
 
     handleFetchRecommendations() {
         const currentUrl = window.location.href; // Get the current URL
-        const payload = {
-            case: {
-                subject: this.caseSubject,
-                description: this.caseDescription,
-                url: currentUrl // Include the current URL in the payload
-            }
-        };
 
-        console.log('Payload:', payload); // Log the payload to verify
-
-        fetch('https://caseybot-3785eca7c1f1.herokuapp.com/match_cases', {
+        fetch('https://your-heroku-app.herokuapp.com/match_cases', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(payload)
+            body: JSON.stringify({
+                case: {
+                    url: currentUrl // Include the current URL in the payload
+                }
+            })
         })
         .then(response => response.json())
         .then(data => {
