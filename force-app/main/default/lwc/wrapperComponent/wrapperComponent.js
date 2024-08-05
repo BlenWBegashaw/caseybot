cf141b507f05dc70ec0612e86d9997df556c8247
BlenWBegashaw <133809716+BlenWBegashaw@users.noreply.github.com>
2024-08-05 13:49:54 -0700

    Update wrapperComponent.js

--- force-app/main/default/lwc/wrapperComponent/wrapperComponent.js
+++ force-app/main/default/lwc/wrapperComponent/wrapperComponent.js
@@ -1,37 +1,37 @@
-import { LightningElement, wire } from 'lwc';
-import { publish, MessageContext } from 'lightning/messageService';
-import SAMPLEMC from '@salesforce/messageChannel/SampleMessageChannel__c';
-import { getRecord } from 'lightning/uiRecordApi';
-import CASE_SUBJECT_FIELD from '@salesforce/schema/Case.Subject';
-import CASE_DESCRIPTION_FIELD from '@salesforce/schema/Case.Description';
+// import { LightningElement, wire } from 'lwc';
+// import { publish, MessageContext } from 'lightning/messageService';
+// import SAMPLEMC from '@salesforce/messageChannel/SampleMessageChannel__c';
+// import { getRecord } from 'lightning/uiRecordApi';
+// import CASE_SUBJECT_FIELD from '@salesforce/schema/Case.Subject';
+// import CASE_DESCRIPTION_FIELD from '@salesforce/schema/Case.Description';
 
-const FIELDS = [CASE_SUBJECT_FIELD, CASE_DESCRIPTION_FIELD];
+// const FIELDS = [CASE_SUBJECT_FIELD, CASE_DESCRIPTION_FIELD];
 
-export default class WrapperComponent extends LightningElement {
-    @wire(MessageContext)
-    messageContext;
+// export default class WrapperComponent extends LightningElement {
+//     @wire(MessageContext)
+//     messageContext;
 
-    @api recordId;
-    caseSubject;
-    caseDescription;
+//     @api recordId;
+//     caseSubject;
+//     caseDescription;
 
-    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
-    case({ error, data }) {
-        if (data) {
-            this.caseSubject = data.fields.Subject.value;
-            this.caseDescription = data.fields.Description.value;
-            this.publishCaseDetails();
-        } else if (error) {
-            console.error(error);
-        }
-    }
+//     @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
+//     case({ error, data }) {
+//         if (data) {
+//             this.caseSubject = data.fields.Subject.value;
+//             this.caseDescription = data.fields.Description.value;
+//             this.publishCaseDetails();
+//         } else if (error) {
+//             console.error(error);
+//         }
+//     }
 
-    publishCaseDetails() {
-        const message = {
-            subject: this.caseSubject,
-            description: this.caseDescription
-        };
-        publish(this.messageContext, SAMPLEMC, message);
-    }
-}
+//     publishCaseDetails() {
+//         const message = {
+//             subject: this.caseSubject,
+//             description: this.caseDescription
+//         };
+//         publish(this.messageContext, SAMPLEMC, message);
+//     }
+// }
 
